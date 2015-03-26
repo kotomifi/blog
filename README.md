b93792e0ae0d49817cb0197b14b94fec5dfa6456
baisu <kotomifi@gmail.com>
2015-03-26 21:14:49 +0800

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+#个人博客系统
+
+书写规则采用markdown语法，初次使用请在mysql中执行main.sql脚本
+
+文件以文章标题命名
+
+开头包含一下部分
+```
+  Date: 2015-03-26 20:02:33
+  Title: I am a title
+  Tags: tag1, tag2
+  Author: xiah
+  Abstract: this is abstract area
+```
+
+文章放在 /post目录下，图片放在/img目录下
---
+++ main.sql
@@ -0,0 +1,28 @@
+-- 创建数据库
+drop database if exists xiah_blog;
+create database xiah_blog DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+use xiah_blog
+
+-- 创建blog表
+create table blog (
+  blog_id  int(12) primary key NOT NULL,
+  blog_date timestamp NOT NULL,
+  blog_title varchar(255) NOT NULL,
+  blog_author varchar(20),
+  blog_abstract text NOT NULL
+);
+
+-- 创建tag表
+create table tag (
+  tag_name varchar(20) primary key NOT NULL,
+  blog_id int(12) NOT NULL,
+  foreign key(blog_id) references blog(blog_id)
+);
+
+-- 创建archive表
+create table archive (
+  archive_name char(4) primary key NOT NULL,
+  blog_id int(12) NOT NULL,
+  foreign key(blog_id) references blog(blog_id)
+);
